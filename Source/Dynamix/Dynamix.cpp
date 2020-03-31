be9285b0d2caa5aa0545457fb8625a2dccef61ac
Riley Wilton <meetingprince34@outlook.com>
2020-03-31 19:15:42 -0400

    initial upload

---
+++ Source/Dynamix/Dynamix.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Dynamix.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dynamix, "Dynamix" );
